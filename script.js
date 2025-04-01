8a4881bdb980255f44c87bb081536724f45c9c03
AnDrew <x2swwwx@gmail.com>
2025-04-01 18:58:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // การจัดการเหตุการณ์การคลิกปุ่ม .btn-primary
+    
     const primaryButtons = document.querySelectorAll(".btn-primary");
     primaryButtons.forEach(button => {
         button.addEventListener("click", function (event) {
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     
 
-    // การจัดการเหตุการณ์การคลิกปุ่มสำหรับโหมดมืด
+    
     const darkModeButtons = document.querySelectorAll("#defaultBtn, #defaultBtn1, #defaultBtn2, #defaultBtn3");
     darkModeButtons.forEach(button => {
         button.addEventListener("click", function() {
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-    // การจัดการเหตุการณ์การคลิกปุ่ม "Next now"
+    
     const nextBtn = document.getElementById("nextBtn");
     if (nextBtn) {
         nextBtn.addEventListener("click", function () {
